1c6ca8b345224cc531b86756d6994f608576ab38
farhan <farhanhoque764@yahoo.com>
2022-07-12 18:20:44 -0500

    added a new file and modified it

---
+++ generateMarkdown.js
@@ -0,0 +1,44 @@
+// generateReadme function populating the README.md
+function generateReadme(answers) {
+    return `
+<h1 align="center">${answers.projectTitle} 👋</h1>
+  
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+
+## Description
+🔍 ${answers.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+💾 ${answers.installation}
+
+## Usage
+💻 ${answers.usage}
+
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+
+## Contributing
+👪 ${answers.contributing}
+
+## Tests
+✏️ ${answers.tests}
+
+## Questions
+✋ ${answers.questions}<br />
+
+_This README was generated with ❤️ by [README-generator](https://github.com/fheezy/Pro-README.git) 🔥🔥🔥_
+    `;
+  }
+  
+  module.exports = generateReadme;
